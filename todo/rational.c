ad31606c8500d87db68fc0f81b1bd11f8d51fc95
Jamesmalo <9jmalo@stpatrick.org>
2018-09-17 13:57:53 -0500

    day 3 work

--- todo/rational.c
+++ todo/rational.c
@@ -51,17 +51,31 @@ struct RationalNum divide(struct RationalNum a, struct RationalNum b ){
 
 //function for adding rational numbers
 struct RationalNum reduce(struct RationalNum a){  //still working on reduce
-    int temp = 1;
-    if(a.numerator > a.denominator){
-        for(int i = 0; i <  ;i++){
-            if(){
-                
-            }
+    int num = a.numerator;
+    int denom = a.denominator;
+    int small;
+    
+    if(num >= denom){
+        small == denom;
+    }
+    else{
+        small == num;
+    }
+
+    int i = 2;
+    while(i <= small){
+        if(num % i && denom % i == 0){
+            return i;
+        }
+        else{
+            i++
         }
     }
-    int num = a.numerator/temp;
-    int denom = a.denominator/temp;
-    double value =
+
+    num = num/i;
+    denom = num/i;
+    double value = num/denom;
+    return value;
 }
 
 int main(){
